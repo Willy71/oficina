1a4fabe880b98fec88ab3ed2141433be58ffd51b
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-03-18 16:48:38 -0300

    Update 1_Gestão de Ordens de Serviço.py

--- pages/1_Gestão de Ordens de Serviço.py
+++ pages/1_Gestão de Ordens de Serviço.py
@@ -187,7 +187,7 @@ if action == "Nova ordem de serviço":
                 data = pd.DataFrame(
                     [
                         {
-                            # '#': obtener_proximo_id(existing_data),
+                            'user id': obtener_proximo_id(existing_data),
                             'Praca': praca,
                             'Carro': carro,
                             'Modelo': modelo,
