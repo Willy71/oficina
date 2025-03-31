168b1b199d215afeac627d1fe98b0664a556bb94
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-03-31 14:32:53 -0300

    Update 7_Gestão com porcentil.py

--- pages/7_Gestão com porcentil.py
+++ pages/7_Gestão com porcentil.py
@@ -1169,7 +1169,7 @@ elif action == "Atualizar ordem existente":
                     quantidade = float(quant_peca_1) if quant_peca_1 else 0
                     custo_unitario = float(valor_peca_1) if valor_peca_1 else 0
                     valor_final = quantidade * custo_unitario * (1 + porcentaje_adicional/100)
-                    st.text("")
+                    st.text("Valor")
                     st.text("")
                     st.text(f"R$ {valor_final:.2f}")
                 except:
