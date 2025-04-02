d8aba85e6feac6d0a82e399e602b92c102623bb2
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-02 17:36:13 -0300

    Update 7_Gestão com porcentil.py

--- pages/7_Gestão com porcentil.py
+++ pages/7_Gestão com porcentil.py
@@ -1641,9 +1641,9 @@ elif action == "Atualizar ordem existente":
             with col160:
                 gold_text("1") 
             with col161:
-                quant_peca_1 = st.text_input("", value=vendor_data["quant_peca_1"], key="update_quant_peca_1")
+                quant_peca_1 = st.text_input("", value=vendor_data["quant_peca_1"], label_visibility="collapsed", key="update_quant_peca_1")
             with col162:
-                desc_peca_1 = st.text_input("", "", label_visibility="collapsed", value=vendor_data["desc_peca_1"], key="update_desc_peca_1")
+                desc_peca_1 = st.text_input("", label_visibility="collapsed", value=vendor_data["desc_peca_1"], label_visibility="collapsed", key="update_desc_peca_1")
             with col163:
                 valor_peca_1 = st.number_input("", value=vendor_data["valor_peca_1"], label_visibility="collapsed", key="update_valor_peca_1")
             with col164:
