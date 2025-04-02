ea61ec344938ada1b18793338897fb07ed16e3a7
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-02 13:38:34 -0300

    Update 7_Gestão com porcentil.py

--- pages/7_Gestão com porcentil.py
+++ pages/7_Gestão com porcentil.py
@@ -530,13 +530,13 @@ if action == "Nova ordem de serviço":
             col170, col171, col172, col173, col174, col175 = st.columns([0.3, 0.5, 3, 0.7, 0.7, 0.7])
             with col170:
                 add_space(2)
-                gold_text("1")     
+                gold_text("2")     
             with col171:
-                quant_peca_2 = st.text_input("", "2",label_visibility="hidden")
+                quant_peca_2 = st.text_input("", "1",label_visibility="hidden")
             with col172:
                 desc_peca_2 = st.text_input("", "", label_visibility="hidden")
             with col173:
-                valor_peca_2 = st.number_input(" ", value=None, label_visibility="hidden")
+                valor_peca_2 = st.number_input("", value=None, label_visibility="hidden")
             with col174: 
                 if quant_peca_2 and valor_peca_2:
                     try:
