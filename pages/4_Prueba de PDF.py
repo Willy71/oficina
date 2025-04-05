92d861b5c39f038651a1009752352751e8d6d54c
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-05 10:43:33 -0300

    Update 4_Prueba de PDF.py

--- pages/4_Prueba de PDF.py
+++ pages/4_Prueba de PDF.py
@@ -92,36 +92,6 @@ def inicializar_hoja():
         st.error(f"Erro ao acessar planilha: {str(e)}")
         return None
 
-# Definir las columnas en el orden correcto
-# Definir el esquema de columnas en el orden correcto
-columnas_ordenadas = ['user_id', 'date_in', 'date_prev', 'date_out', 'carro', 'modelo', 'cor', 'placa', 'km', 'ano', 
-                      'estado', 'dono_empresa', 'telefone', 'endereco', 'item_serv_1', 'desc_ser_1', 'valor_serv_1',
-                      'item_serv_2', 'desc_ser_2', 'valor_serv_2', 'item_serv_3', 'desc_ser_3', 'valor_serv_3',
-                      'item_serv_4', 'desc_ser_4', 'valor_serv_4', 'item_serv_5', 'desc_ser_5', 'valor_serv_5',
-                      'item_serv_6', 'desc_ser_6', 'valor_serv_6', 'item_serv_7', 'desc_ser_7', 'valor_serv_7',
-                      'item_serv_8', 'desc_ser_8', 'valor_serv_8', 'item_serv_9', 'desc_ser_9', 'valor_serv_9',
-                      'item_serv_10', 'desc_ser_10', 'valor_serv_10', 'item_serv_11', 'desc_ser_11', 'valor_serv_11',
-                      'item_serv_12', 'desc_ser_12', 'valor_serv_12', 'total_serviço', 'porcentaje_adicional',
-                      'quant_peca_1', 'desc_peca_1', 'valor_peca_1', 'sub_tota_peca_1', 'valor_total_peca_1', 
-                      'quant_peca_2', 'desc_peca_2', 'valor_peca_2', 'sub_tota_peca_2','valor_total_peca_2',
-                      'quant_peca_3', 'desc_peca_3', 'valor_peca_3', 'sub_tota_peca_3', 'valor_total_peca_3',
-                      'quant_peca_4', 'desc_peca_4', 'valor_peca_4', 'sub_tota_peca_4', 'valor_total_peca_4',
-                      'quant_peca_5', 'desc_peca_5', 'valor_peca_5', 'sub_tota_peca_5', 'valor_total_peca_5',
-                      'quant_peca_6', 'desc_peca_6', 'valor_peca_6', 'sub_tota_peca_6', 'valor_total_peca_6',
-                      'quant_peca_7', 'desc_peca_7', 'valor_peca_7', 'sub_tota_peca_7', 'valor_total_peca_7',
-                      'quant_peca_8', 'desc_peca_8', 'valor_peca_8', 'sub_tota_peca_8', 'valor_total_peca_8',
-                      'quant_peca_9', 'desc_peca_9', 'valor_peca_9', 'sub_tota_peca_9', 'valor_total_peca_9',
-                      'quant_peca_10', 'desc_peca_10', 'valor_peca_10', 'sub_tota_peca_10', 'valor_total_peca_10',
-                      'quant_peca_11', 'desc_peca_11', 'valor_peca_11', 'sub_tota_peca_11', 'valor_total_peca_11',
-                      'quant_peca_12', 'desc_peca_12', 'valor_peca_12', 'sub_tota_peca_12', 'valor_total_peca_12',
-                      'quant_peca_13', 'desc_peca_13', 'valor_peca_13', 'sub_tota_peca_13', 'valor_total_peca_13',
-                      'quant_peca_14', 'desc_peca_14', 'valor_peca_14', 'sub_tota_peca_14', 'valor_total_peca_14',
-                      'quant_peca_15', 'desc_peca_15', 'valor_peca_15', 'sub_tota_peca_15', 'valor_total_peca_15',
-                      'quant_peca_16', 'desc_peca_16', 'valor_peca_16', 'sub_tota_peca_16', 'valor_total_peca_16',
-                      'total_costo_inicial', 'total_costo_final', 'forma_de_pagamento', 'pagamento_parcial', 
-                      'valor_pago_parcial', 'data_prox_pag', 'valor_prox_pag', 'pag_total', 'valor_pag_total'
-                     ]
-
 # Función para cargar datos desde Google Sheets
 def cargar_datos(worksheet):
     try:
