6c0f5b6d981df6e70a65647c43eb69740c46a5b3
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-05 10:05:00 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,12 +21,12 @@ reduce_space ="""
             </style>
             """
 # We load reduce_space
-st.html(reduce_space)
+st.markdown(reduce_space, unsafe_allow_html=True)
 
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://i.postimg.cc/7LZ9Nqw6/jr-korpa-H-BJWTh-ZRok-unsplash.jpg");
+background-image: url("https://github.com/Willy71/oficina/blob/main/pictures/wallpaper%20black%20vintage.jpg?raw=true");
 background-size: 180%;
 background-position: top left;
 background-repeat: repeat;
