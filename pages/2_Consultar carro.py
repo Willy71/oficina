cf15e0b5d6f2dbc3bbe2535704a3fc8bdaef8da1
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-05 09:51:26 -0300

    Update 2_Consultar carro.py

--- pages/2_Consultar carro.py
+++ pages/2_Consultar carro.py
@@ -37,7 +37,7 @@ st.markdown(reduce_space, unsafe_allow_html=True)
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://i.postimg.cc/jdtSsJ9t/jr-korpa-H-BJWTh-ZRok-unsplash.jpg");
+background-image: url("https://github.com/Willy71/oficina/blob/main/pictures/wallpaper%20black%20vintage.jpg?raw=true");
 background-size: 180%;
 background-position: top left;
 background-repeat: repeat;
