76ad2c614dbf473107b4bbfb32c553b9b02130fa
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-06 11:28:58 -0300

    Update 1_Gestão de ordens de serviço.py

--- pages/1_Gestão de ordens de serviço.py
+++ pages/1_Gestão de ordens de serviço.py
@@ -607,11 +607,11 @@ if action == "Nova ordem de serviço":
                     gold_text("R$ 0.00")                
 
 	with st.container():
-	    col180, col181, col182, col183, col184, col185 = st.columns([0.3, 0.5, 3, 0.7, 0.7, 0.7])
-            with col180:
-                gold_text("3")
-	    with col181:
-		quant_peca_3_str = st.text_input("", "1", label_visibility="collapsed", key="quant_peca_3")
+            col180, col181, col182, col183, col184, col185 = st.columns([0.3, 0.5, 3, 0.7, 0.7, 0.7])
+            with col181:
+                gold_text("3")     
+            with col171:
+                quant_peca_3_str = st.text_input("", "1", label_visibility="collapsed", key="quant_peca_3")
                 quant_peca_3 = parse_br_number(quant_peca_3_str)  
             with col182:
                 desc_peca_3 = st.text_input("", "", label_visibility="collapsed", key="desc_peca_3")
@@ -628,6 +628,7 @@ if action == "Nova ordem de serviço":
                 else:
                     gold_text("R$ 0.00")
             with col185:
+                # Mostrar costo final (con porcentaje aplicado)
                 if quant_peca_3 and valor_peca_3 and porcentaje_adicional:
                     try:
                         costo_final_3 = float(quant_peca_3) * float(valor_peca_3) * (1 + porcentaje_adicional/100)
@@ -635,7 +636,7 @@ if action == "Nova ordem de serviço":
                     except:
                         gold_text("R$ 0.00")
                 else:
-                    gold_text("R$ 0.00")
+                    gold_text("R$ 0.00")  
         
         with st.container():
             col190, col191, col192, col193, col194, col195 = st.columns([0.3, 0.5, 3, 0.7, 0.7, 0.7])
