3ca3e7cfd54c038683c9150cb8a6a1923617dfe9
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-06 11:48:50 -0300

    Update 1_Gestão de ordens de serviço.py

--- pages/1_Gestão de ordens de serviço.py
+++ pages/1_Gestão de ordens de serviço.py
@@ -605,14 +605,14 @@ if action == "Nova ordem de serviço":
                         gold_text("R$ 0.00")
                 else:
                     gold_text("R$ 0.00")
-
+	
 	with st.container():
-		col180, col181, col182, col183, col184, col185 = st.columns([0.3, 0.5, 3, 0.7, 0.7, 0.7])
-		with col181:
-			gold_text("3")
-		with col171:
-			quant_peca_3_str = st.text_input("", "1", label_visibility="collapsed", key="quant_peca_3")
-			quant_peca_3 = parse_br_number(quant_peca_3_str)  
+	    col180, col181, col182, col183, col184, col185 = st.columns([0.3, 0.5, 3, 0.7, 0.7, 0.7])
+	    with col181:
+		gold_text("3")
+	    with col171:
+		quant_peca_3_str = st.text_input("", "1", label_visibility="collapsed", key="quant_peca_3")
+		quant_peca_3 = parse_br_number(quant_peca_3_str)  
 		with col182:
 			desc_peca_3 = st.text_input("", "", label_visibility="collapsed", key="desc_peca_3")
 		with col183:
