73d3249761307e3278db5f49fe95bcb9084e9384
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-06 12:01:54 -0300

    Update 1_Gestão de ordens de serviço.py

--- pages/1_Gestão de ordens de serviço.py
+++ pages/1_Gestão de ordens de serviço.py
@@ -605,7 +605,43 @@ if action == "Nova ordem de serviço":
                         gold_text("R$ 0.00")
                 else:
                     gold_text("R$ 0.00")
-	
+		
+        with st.container():
+            col180, col181, col182, col183, col184, col185 = st.columns([0.3, 0.5, 3, 0.7, 0.7, 0.7])
+		    with col181:
+		        gold_text("3")
+                
+		    with col171:
+				quant_peca_3_str = st.text_input("", "1", label_visibility="collapsed", key="quant_peca_3")
+				quant_peca_3 = parse_br_number(quant_peca_3_str)  
+                
+			with col182:
+				desc_peca_3 = st.text_input("", "", label_visibility="collapsed", key="desc_peca_3")
+                
+			with col183:
+				valor_peca_3_str = st.text_input(" ", value="0,00", label_visibility="collapsed", key="valor_peca_3")
+				valor_peca_3 = parse_br_number(valor_peca_3_str)
+                
+			with col184: 
+				if quant_peca_3 and valor_peca_3:
+					try:
+						costo_inicial_3 = float(quant_peca_3) * float(valor_peca_3)
+						gold_text(f"R$ {costo_inicial_3:.2f}")
+					except:
+						gold_text("R$ 0.00")
+				else:
+					gold_text("R$ 0.00")
+                    
+			with col185:
+				# Mostrar costo final (con porcentaje aplicado)
+				if quant_peca_3 and valor_peca_3 and porcentaje_adicional:
+					try:
+						costo_final_3 = float(quant_peca_3) * float(valor_peca_3) * (1 + porcentaje_adicional/100)
+						gold_text(f"R$ {costo_final_3:.2f}")        
+					except:
+						gold_text("R$ 0.00")
+				else:
+					gold_text("R$ 0.00")
 
         
         with st.container():
