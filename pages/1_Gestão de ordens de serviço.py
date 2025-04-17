2e7e7ade712b42c210faad5442e0f9fc9f1e46cb
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-17 13:24:24 -0300

    Update 1_Gestão de ordens de serviço.py

--- pages/1_Gestão de ordens de serviço.py
+++ pages/1_Gestão de ordens de serviço.py
@@ -1210,12 +1210,10 @@ elif action == "Atualizar ordem existente":
                         else:
                             with col202:
                                 st.write("")  # Espaciador
-                                st.write("")  # Espaciador
                                 st.warning("Nenhuma ordem de serviço encontrada com essa placa.")
                                 st.stop()
                     else:
                         with col202:
-                            st.write("")  # Espaciador
                             st.write("")  # Espaciador
                             st.warning("Digite o número da placa para pesquisar e pressione Enter.")
                             st.stop()
