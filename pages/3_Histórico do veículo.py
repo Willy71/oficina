2c727b7b4cf1a725388b4ae88bd1cf9ece69656f
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-17 12:48:24 -0300

    Update 3_Histórico do veículo.py

--- pages/3_Histórico do veículo.py
+++ pages/3_Histórico do veículo.py
@@ -55,7 +55,7 @@ with st.container():
     with col1:
         placa_input = st.text_input("Digite a placa do veículo:", key="placa_hist_input").strip().upper()
     with col2:
-        st.write("")  # espacio
+        st.text("")  # espacio
         buscar = st.button("Buscar", key="buscar_historico_btn")
 
 if buscar:
