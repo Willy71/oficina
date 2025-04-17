9ca2ba5e3484204bd68d05317292a2167fd71180
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-17 10:01:38 -0300

    Update 2_Consultar ordem.py

--- pages/2_Consultar ordem.py
+++ pages/2_Consultar ordem.py
@@ -282,21 +282,21 @@ if st.session_state.veiculo_encontrado:
         cols = st.columns(4)
         with cols[0]:
             center_text("Placa", 6, "yellow")
-            center_text(f"{formatar_valor(veiculo.get('placa'))}", 5, "white")
+            center_text(f"{formatar_valor(veiculo.get('placa'))}", 4, "white")
         with cols[1]:
             center_text("Marca", 6, "yellow")
-            center_text(f"{formatar_valor(veiculo.get('carro'))}", 5, "white")
+            center_text(f"{formatar_valor(veiculo.get('carro'))}", 4, "white")
             #st.metric("Marca", formatar_valor(veiculo.get('carro')))
         with cols[2]:
             center_text("Modelo", 6, "yellow")
-            center_text(f"{formatar_valor(veiculo.get('modelo'))}", 5, "white")
+            center_text(f"{formatar_valor(veiculo.get('modelo'))}", 4, "white")
             #st.metric("Modelo", formatar_valor(veiculo.get('modelo')))
         with cols[3]:
             ano = veiculo.get('ano')
             if isinstance(ano, float):
                 ano = int(ano)
             center_text("Ano", 6, "yellow")
-            center_text(f"{formatar_valor(veiculo.get('ano'))}", 5, "white")
+            center_text(f"{formatar_valor(veiculo.get('ano'))}", 4, "white")
             #st.metric("Ano", formatar_valor(ano))
     
     # Mostrar detalles del estado y fechas
