c24235cd91a17e162bed8c397b8d6b1d9a0a1daa
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-17 12:10:49 -0300

    Update 6_Control de Stock.py

--- pages/6_Control de Stock.py
+++ pages/6_Control de Stock.py
@@ -97,10 +97,10 @@ st.header("🛒 Registrar Venda")
 
 if not df.empty:
     # Ordenar el DataFrame por 'descripcao'
-    df_ordenado = df.sort_values(by="descripcao", ascending=True)
+    #df_ordenado = df.sort_values(by="descripcao", ascending=True)
     # Crear la lista desplegable a partir del DataFrame ya ordenado
-    #produtos = df['descripcao'] + " | Código: " + df['id_prod'].astype(str)
-    produtos = df_ordenado['descripcao'] + " | Código: " + df_ordenado['id_prod'].astype(str)
+    produtos = df['descripcao'] + " | Código: " + df['id_prod'].astype(str)
+    #produtos = df_ordenado['descripcao'] + " | Código: " + df_ordenado['id_prod'].astype(str)
     produto_escolhido = st.selectbox("Produto", produtos)
     
  
