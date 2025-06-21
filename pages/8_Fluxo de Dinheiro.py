462805b7474d119504d837e3414c8e0af128380c
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-06-21 09:43:58 -0300

    Update 8_Fluxo de Dinheiro.py

--- pages/8_Fluxo de Dinheiro.py
+++ pages/8_Fluxo de Dinheiro.py
@@ -262,7 +262,7 @@ with aba2:
 
     st.markdown("### 📋 Filtrar lançamentos por tipo")
 
-    col1, col2, col3, col4, col5, col6, col7, col8, col9, col10 = st.columns([3,0.5,3,0.5,3,0.5,3,2,2,2])
+    col1, col2, col3, col4, col5, col6, col7, col8, col9, col10 = st.columns([3,0.5,3,0.2,3,0.5,3,2,2,2])
     mostrar_tipo = None
     with col1:
         if st.button("🟢 Entradas", key="btn_lan_entradas"):
