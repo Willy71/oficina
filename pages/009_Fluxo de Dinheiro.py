e91bae421da76862fa3fe8cb462b5369036466a4
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-07-15 15:50:32 -0300

    Update 009_Fluxo de Dinheiro.py

--- pages/009_Fluxo de Dinheiro.py
+++ pages/009_Fluxo de Dinheiro.py
@@ -429,10 +429,15 @@ with aba4:
         else:
             primeiro_dia = data_min
             ultimo_dia = data_max
+		
+		
+			
+			
 		#=====================================================================	
 		if (ultimo_dia < data_min) or (primeiro_dia > data_max):
 			st.warning(f"⚠️ Nenhum dado encontrado para {meses[mes_selecionado]} de {ano_selecionado}.")
 			st.stop()
+
 
         # Corrige datas fora do intervalo permitido
         data_inicio_padrao = max(min(primeiro_dia, data_max), data_min)
