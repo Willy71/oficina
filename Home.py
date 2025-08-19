964ac814378b190bc6f0daa54166a29ffb4d94bd
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-08-19 13:51:27 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -26,7 +26,7 @@ st.markdown(reduce_space, unsafe_allow_html=True)
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://github.com/Willy71/oficina/blob/main/pictures/wallpaper%20black%20vintage.jpg?raw=true");
+background-image: url("https://github.com/Willy71/oficina/blob/main/pictures/image.jpg?raw=true");
 background-size: 180%;
 background-position: top left;
 background-repeat: repeat;
