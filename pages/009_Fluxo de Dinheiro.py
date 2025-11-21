cc6d9f787a25d863dbed1e2cc78116aa208234c1
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-11-21 09:02:55 -0300

    Fix indentation for input fields in Fluxo de Dinheiro

--- pages/009_Fluxo de Dinheiro.py
+++ pages/009_Fluxo de Dinheiro.py
@@ -244,8 +244,8 @@ with aba1:
         with cols[1]:
             cliente = st.text_input("Cliente")
 
-        descricao = st.text_input("Descrição")
-		categoria = st.selectbox("Categoria", categorias)
+    descricao = st.text_input("Descrição")
+	categoria = st.selectbox("Categoria", categorias)
 
         cols = st.columns(4)
         with cols[1]:
