8b54a29f65e38f90e6efbe3f7ccd4ab9e81753c7
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2026-01-12 17:00:45 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -4,7 +4,13 @@ from firebase_admin import credentials, firestore
 import streamlit as st
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(dict(st.secrets["firebase"]))
+    firebase_dict = dict(st.secrets["firebase"])
+
+    # Normalizar la private_key por si viene con \n escapados
+    if "private_key" in firebase_dict:
+        firebase_dict["private_key"] = firebase_dict["private_key"].replace("\\n", "\n")
+
+    cred = credentials.Certificate(firebase_dict)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
